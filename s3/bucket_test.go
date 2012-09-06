65bd5e9669ada97cba26d2e1efed52c1d7586549
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-09-06 16:04:40 +1000

    Added a test stub.

---
+++ s3/bucket_test.go
@@ -0,0 +1,57 @@
+// Copyright 2012 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package s3
+
+import (
+	. "github.com/jacobsa/ogletest"
+	"testing"
+)
+
+func TestBucket(t *testing.T) { RunTests(t) }
+
+////////////////////////////////////////////////////////////////////////
+// Helpers
+////////////////////////////////////////////////////////////////////////
+
+type bucketTest struct {
+	httpConn mock_http.MockConn
+	signer mock_auth.MockSigner
+	bucket Bucket
+}
+
+func (t *ConnTest) SetUp(i *TestInfo) {
+	var err error
+
+	t.httpConn = mock_http.NewMockConn(i.MockController, "httpConn")
+	t.signer = mock_auth.NewMockSigner(i.MockController, "signer")
+	t.bucket, err = openBucket("some.bucket", t.httpConn, t.signer)
+	AssertEq(nil, err)
+}
+}
+
+////////////////////////////////////////////////////////////////////////
+// Storebject
+////////////////////////////////////////////////////////////////////////
+
+type StorebjectTest {
+	bucketTest
+}
+
+func init() { RegisterTestSuite(&StorebjectTest{}) }
+
+func (t *StorebjectTest) DoesFoo() {
+	ExpectEq("TODO", "")
+}
