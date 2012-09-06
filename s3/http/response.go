da2337c4619ec38ef29a2b5f693380a26c877c19
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-09-06 14:24:08 +1000

    Added an HTTP response struct.

---
+++ s3/http/response.go
@@ -0,0 +1,25 @@
+// Copyright 2012 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package http
+
+// An HTTP response from S3.
+type Response struct {
+	// The HTTP status code, e.g. 200 or 404.
+	StatusCode int
+
+	// The response body. This is the empty slice if the body was empty.
+	Body []byte
+}
