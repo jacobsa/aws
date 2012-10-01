484a205a14d8b39c2b8e3c1df0103e454e75e35c
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-01 11:59:34 +1000

    ListKeysTest.ResponseContainsNoKeys

--- s3/bucket_test.go
+++ s3/bucket_test.go
@@ -634,7 +634,29 @@ func (t *ListKeysTest) WrongRootTag() {
 }
 
 func (t *ListKeysTest) ResponseContainsNoKeys() {
-	ExpectFalse(true, "TODO")
+	min := ""
+
+	// Signer
+	ExpectCall(t.signer, "Sign")(Any()).
+		WillOnce(oglemock.Return(nil))
+
+	// Conn
+	resp := &http.Response{
+		StatusCode: 200,
+		Body:       []byte(`
+			<?xml version="1.0" encoding="UTF-8"?>
+			<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+			</ListBucketResult>`),
+	}
+
+	ExpectCall(t.httpConn, "SendRequest")(Any()).
+		WillOnce(oglemock.Return(resp, nil))
+
+	// Call
+	keys, err := t.bucket.ListKeys(min)
+	AssertEq(nil, err)
+
+	ExpectThat(keys, ElementsAre())
 }
 
 func (t *ListKeysTest) ResponseContainsSomeKeys() {
