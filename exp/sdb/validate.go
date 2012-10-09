d76d3ff802a41d9e4f7910c702b3e60f5eec6945
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-09 22:05:19 +1100

    Implemented validateValue.

--- exp/sdb/validate.go
+++ exp/sdb/validate.go
@@ -17,11 +17,38 @@ package sdb
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
+
+func isLegalXmlCharacter(r rune) (inrange bool) {
+	return r == 0x09 ||
+		r == 0x0A ||
+		r == 0x0D ||
+		(r >= 0x20 && r <= 0xDF77) ||
+		(r >= 0xE000 && r <= 0xFFFD) ||
+		(r >= 0x10000 && r <= 0x10FFFF)
+}
 
 // Return an error iff the supplied value is invalid when used as an item or
 // attribute name or value. For generality, the empty string is allowed by this
 // function.
 func validateValue(val string) error {
-	return fmt.Errorf("TODO")
+	// Values may be no more than 1024 bytes long.
+	if len(val) > 1024 {
+		return fmt.Errorf("Longer than 1024 bytes.")
+	}
+
+	// Values must be valid UTF-8.
+	if !utf8.ValidString(val) {
+		return fmt.Errorf("Not valid UTF-8.")
+	}
+
+	// Each codepoint in the string must be legal in an XML 1.0 document.
+	for _, r := range val {
+		if !isLegalXmlCharacter(r) {
+			return fmt.Errorf("Invalid codepoint for XML 1.0 document: %U", r)
+		}
+	}
+
+	return nil
 }
