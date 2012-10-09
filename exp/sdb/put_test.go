11e1857efe8fe82fb0ae6221b5be013c58d3fdb5
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-09 21:22:31 +1100

    Added test names.

--- exp/sdb/put_test.go
+++ exp/sdb/put_test.go
@@ -28,11 +28,57 @@ func TestPut(t *testing.T) { RunTests(t) }
 
 type PutTest struct {
 	domainTest
+
+	item ItemName
+	updates []PutUpdate
+	preconditions []Precondition
+
+	err error
 }
 
 func init() { RegisterTestSuite(&PutTest{}) }
 
-func (t *PutTest) DoesFoo() {
+func (t *PutTest) InvalidItemName() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) ZeroUpdates() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) TooManyUpdates() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) OneAttributeNameInvalid() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) OneAttributeValueInvalid() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) OnePreconditionNameInvalid() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) OnePreconditionValueInvalid() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) NoPreconditions() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) SomePreconditions() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) ConnReturnsError() {
+	ExpectEq("TODO", "")
+}
+
+func (t *PutTest) ConnSaysOkay() {
 	ExpectEq("TODO", "")
 }
 
